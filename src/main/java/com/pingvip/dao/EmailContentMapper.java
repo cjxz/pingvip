01cc4e6609566c46892c4c1fbf7896fd62a85a0b
chao.zhu <chao.zhu@rograndec.com>
2017-09-21 13:26:24 +0800

    邮件插件DAO层

---
+++ src/main/java/com/pingvip/dao/EmailContentMapper.java
@@ -0,0 +1,20 @@
+package com.pingvip.dao;
+
+
+import com.pingvip.entity.EmailContent;
+
+public interface EmailContentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(EmailContent record);
+
+    int insertSelective(EmailContent record);
+
+    EmailContent selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(EmailContent record);
+
+    int updateByPrimaryKeyWithBLOBs(EmailContent record);
+
+    int updateByPrimaryKey(EmailContent record);
+}
---
+++ src/main/java/com/pingvip/entity/EmailContent.java
@@ -0,0 +1,55 @@
+package com.pingvip.entity;
+
+import java.util.Date;
+
+public class EmailContent {
+    private Integer id;
+
+    private String name;
+
+    private String title;
+
+    private Date createTime;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
